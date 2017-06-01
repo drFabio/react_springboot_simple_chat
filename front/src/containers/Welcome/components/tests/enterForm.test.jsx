2af06a1276cbda1e7bbc48e6495a3d00c3c4054e
drFabio <blackjackdevel@gmail.com>
2017-06-01 19:04:26 -0300

    Added Enter form failing tests

--- front/src/containers/Welcome/components/tests/enterForm.test.jsx
+++ front/src/containers/Welcome/components/tests/enterForm.test.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 import {EnterForm} from '../enterForm'
 
 describe('<Enterform />', () => {
-  it('renders', () => {
+  it('renders an input', () => {
     const renderedComponent = shallow(<EnterForm />)
-    expect(renderedComponent).toMatchSnapshot()
+    expect(renderedComponent.type()).toEqual('input')
+  })
+  it('Disables if entering prop is passed ', () => {
+    const renderedComponent = shallow(<EnterForm entering />)
+    expect(renderedComponent.prop('disabled')).toEqual(true)
   })
 })
