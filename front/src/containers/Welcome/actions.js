a0d5fd99152262cbfb56c43e476e64a9a9f25099
drFabio <blackjackdevel@gmail.com>
2017-06-01 17:20:15 -0300

    Action failing tests added

---
+++ front/src/containers/Welcome/actions.js
@@ -0,0 +1,14 @@
+export function enterChat () {
+  return {}
+}
+export function userAlreadyExists () {
+  return {}
+}
+export function welcomeToTheChat () {
+  return {}
+}
+export default {
+  enterChat,
+  userAlreadyExists,
+  welcomeToTheChat
+}
---
+++ front/src/containers/Welcome/tests/actions.test.js
@@ -0,0 +1,27 @@
+import types from '../actionTypes'
+import actions from '../actions'
+describe('Welcome container Actions', () => {
+  it('enterChat returns ENTER_CHAT with userName data', () => {
+    const mockName = 'mockName'
+    const expectedResult = {
+      type: types.ENTER_CHAT,
+      userName: mockName
+    }
+    expect(actions.enterChat(mockName))
+      .toEqual(expectedResult)
+  })
+  it('user already exists returns USER_ALREADY_EXISTS', () => {
+    const expectedResult = {
+      type: types.USER_ALREADY_EXISTS
+    }
+    expect(actions.userAlreadyExists())
+      .toEqual(expectedResult)
+  })
+  it('welcomeToTheChat returns WELCOME_TO_THE_CHAT', () => {
+    const expectedResult = {
+      type: types.WELCOME_TO_THE_CHAT
+    }
+    expect(actions.welcomeToTheChat())
+      .toEqual(expectedResult)
+  })
+})
