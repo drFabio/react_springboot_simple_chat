abdefb2d26737f9f20711fe15de23cf1891ede95
drFabio <blackjackdevel@gmail.com>
2017-06-01 19:12:27 -0300

    Added enterForm tests

--- front/src/containers/Welcome/components/tests/enterForm.test.jsx
+++ front/src/containers/Welcome/components/tests/enterForm.test.jsx
@@ -1,10 +1,16 @@
 import { shallow } from 'enzyme'
 import React from 'react'
-import {EnterForm} from '../enterForm'
+import {EnterForm, EnterButton, NameInput} from '../enterForm'
 
 describe('<Enterform />', () => {
-  it('renders', () => {
+  it('renders one EnterButton and one NameInput', () => {
     const renderedComponent = shallow(<EnterForm />)
-    expect(renderedComponent).toMatchSnapshot()
+    expect(renderedComponent.find(EnterButton).length).toEqual(1)
+    expect(renderedComponent.find(NameInput).length).toEqual(1)
+  })
+  it('Pass entering property down', () => {
+    const renderedComponent = shallow(<EnterForm entering />)
+    expect(renderedComponent.first(EnterButton).prop('entering')).toEqual(true)
+    expect(renderedComponent.first(NameInput).prop('entering')).toEqual(true)
   })
 })
