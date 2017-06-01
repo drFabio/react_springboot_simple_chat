1a841890a9c8363d1de9ae92888794ea6bfa3e28
drFabio <blackjackdevel@gmail.com>
2017-06-01 17:22:14 -0300

    Actions implemented

--- front/src/containers/Welcome/actions.js
+++ front/src/containers/Welcome/actions.js
@@ -1,11 +1,19 @@
-export function enterChat () {
-  return {}
+import types from './actionTypes'
+export function enterChat (userName) {
+  return {
+    type: types.ENTER_CHAT,
+    userName
+  }
 }
 export function userAlreadyExists () {
-  return {}
+  return {
+    type: types.USER_ALREADY_EXISTS
+  }
 }
 export function welcomeToTheChat () {
-  return {}
+  return {
+    type: types.WELCOME_TO_THE_CHAT
+  }
 }
 export default {
   enterChat,
