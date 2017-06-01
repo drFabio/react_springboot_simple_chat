1335235b15db38f6cfdbc1ec23475acd2fa0190b
drFabio <blackjackdevel@gmail.com>
2017-06-01 19:00:59 -0300

    Expanded enterButton tests

--- front/src/containers/Welcome/components/tests/enterButton.test.jsx
+++ front/src/containers/Welcome/components/tests/enterButton.test.jsx
@@ -3,8 +3,17 @@ import React from 'react'
 import {EnterButton} from '../enterButton'
 
 describe('<EnterButton />', () => {
-  it('renders', () => {
+  it('renders a button', () => {
     const renderedComponent = shallow(<EnterButton />)
-    expect(renderedComponent).toMatchSnapshot()
+    expect(renderedComponent.type()).toEqual('button')
+  })
+  it('Displays enter if no props is passed ', () => {
+    const renderedComponent = shallow(<EnterButton />)
+    expect(renderedComponent.text()).toEqual('Enter')
+  })
+  it('Displays "Entering ..." and disables if entering prop is passed ', () => {
+    const renderedComponent = shallow(<EnterButton entering />)
+    expect(renderedComponent.text()).toEqual('Entering..')
+    expect(renderedComponent.prop('disabled')).toBeDefined()
   })
 })
