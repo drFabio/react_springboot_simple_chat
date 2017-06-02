b8245e37d94b16a641867ed573b45f4b7bf7838f
drFabio <blackjackdevel@gmail.com>
2017-06-01 21:51:06 -0300

    Connected component test added

---
+++ front/src/containers/Welcome/tests/connectedComponent.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import configureStore from 'redux-mock-store'
+import { shallow } from 'enzyme'
+import {Welcome} from '../index'
+import {Map} from 'immutable'
+describe('<Welcome /> - Connected page', () => {
+  const mockStore = configureStore()
+  it('Renders and pass down the appropriate properties', () => {
+    const initialState = {
+      welcome: new Map({
+        entering: true,
+        error: {'message': 'Foo var'}
+      })
+    }
+    const store = mockStore(initialState)
+    const renderedComponent = shallow(<Welcome store={store} />)
+    expect(renderedComponent.prop('entering')).toEqual(initialState.welcome.get('entering'))
+    expect(renderedComponent.prop('error')).toEqual(initialState.welcome.get('error'))
+  })
+})
