23495fd54fa8c3fcdb190134e42fe3cc26e1412a
drFabio <blackjackdevel@gmail.com>
2017-06-01 21:41:33 -0300

    Connected welcome component

--- front/src/containers/Welcome/index.jsx
+++ front/src/containers/Welcome/index.jsx
@@ -1,12 +1,21 @@
-import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import actions from './actions'
+import {WelcomePage} from './components'
 
-export class Welcome extends Component {
-  render () {
-    return (
-      <div>
-        <h1>Welcome to the chat example</h1>
-      </div>
-    )
+function mapStateToProps (state) {
+  const myState = state.welcome
+  console.log(state.welcome.toJS())
+  return {
+    entering: myState.get('entering'),
+    error: myState.get('error')
   }
 }
+function mapDispatchToProps (dispatch, ownProps) {
+  return {
+    onEnterChat (name) {
+      dispatch(actions.enterChat(name))
+    }
+  }
+}
+export const Welcome = connect(mapStateToProps, mapDispatchToProps)(WelcomePage)
 export default Welcome
