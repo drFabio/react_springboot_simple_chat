7950d45d9e0919db2786cc40bc6dd10d83e61bf5
drFabio <blackjackdevel@gmail.com>
2017-06-02 11:23:20 -0300

    Added chat container action types

---
+++ front/src/containers/Chat/actionTypes.js
@@ -0,0 +1,11 @@
+const prefix = 'chat'
+export const SOMEONE_ENTERED = `${prefix}_SOMEONE_ENTERED`
+export const SOMEONE_LEFT = `${prefix}_SOMEONE_LEFT`
+export const SEND_MESSAGE = `${prefix}_SEND_MESSAGE`
+export const RECEIVED_MESSAGE = `${prefix}_RECEIVED_MESSAGE`
+export default {
+  SOMEONE_ENTERED,
+  RECEIVED_MESSAGE,
+  SOMEONE_LEFT,
+  SEND_MESSAGE
+}
