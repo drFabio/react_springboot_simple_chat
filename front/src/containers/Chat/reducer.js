1b4af08bbc94994d331da1c8152b581f9d9b1b74
drFabio <blackjackdevel@gmail.com>
2017-06-02 16:38:38 -0300

    Implemented chat reducer tests

--- front/src/containers/Chat/reducer.js
+++ front/src/containers/Chat/reducer.js
@@ -1,11 +1,19 @@
 import { Map, List } from 'immutable'
-// import types from './actionTypes'
+import types from './actionTypes'
 
 export const initial = new Map({
   messagesList: new List()
 })
 function chatReducer (state = initial, action) {
   switch (action.type) {
+    case types.SOMEONE_ENTERED:
+      return state.update('messagesList', messages => messages.push(`${action.userName} joined the room`))
+    case types.SOMEONE_LEFT:
+      return state.update('messagesList', messages => messages.push(`${action.userName} left the room`))
+    case types.RECEIVED_MESSAGE:
+      return state.update('messagesList', messages => messages.push(`${action.userName} - : ${action.message}`))
+    case types.SEND_MESSAGE:
+      return state.set('outgoingMessage', action.message)
     default:
       return state
   }
