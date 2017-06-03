9a044d2b08c1a3230fed3dbb1f3a23575c47434a
drFabio <blackjackdevel@gmail.com>
2017-06-03 12:14:02 -0300

    Message form tests added

---
+++ front/src/containers/Chat/components/tests/messageForm.test.js
@@ -0,0 +1,33 @@
+import { shallow, mount } from 'enzyme'
+import React from 'react'
+import {MessageInput, MessageForm, SendMessageButton} from '../'
+import sinon from 'sinon'
+
+describe('<MessageForm />', () => {
+  it('renders one SendMessageButton and one MessageInput', () => {
+    const renderedComponent = shallow(<MessageForm />)
+    expect(renderedComponent.find(SendMessageButton).length).toEqual(1)
+    expect(renderedComponent.find(MessageInput).length).toEqual(1)
+  })
+  it('Fills the input value based on the state', () => {
+    const renderedComponent = mount(<MessageForm />)
+    const mockValue = 'mockValue'
+    renderedComponent.setState({'message': mockValue})
+    expect(renderedComponent.first(MessageInput).find('input').prop('value')).toEqual(mockValue)
+  })
+  it('Save the value of name on state when it changes', () => {
+    const renderedComponent = mount(<MessageForm />)
+    const mockValue = 'mockValue'
+    const mockEvent = {target: {value: mockValue}}
+    renderedComponent.first(MessageInput).find('input').simulate('change', mockEvent)
+    expect(renderedComponent.state('message')).toEqual(mockValue)
+  })
+  it('When the button is send sendMessage prop is invoked and message state is cleared', () => {
+    const sendMessageSpy = sinon.spy()
+    const renderedComponent = mount(<MessageForm sendMessage={sendMessageSpy} />)
+    const mockValue = 'mockValue'
+    renderedComponent.setState({'message': mockValue})
+    renderedComponent.first(SendMessageButton).find('button').simulate('click')
+    expect(renderedComponent.state('message')).toEqual('')
+  })
+})
